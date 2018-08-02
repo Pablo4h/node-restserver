72b29d51c76edcad3b3b405c58d33dc87442a698
pablo <pccm4h@hotmail.com>
2018-08-02 12:40:57 -0500

    RestServer-usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 //============
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//============
+//Entorno
+//============
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//============
+//Base de datos
+//============
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe_user:yondaimehokage0@ds143330.mlab.com:43330/cafe_pruebas';
+}
+
+process.env.URLDB = urlDB;
